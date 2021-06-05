7ba4b62dd9f9a32cdab6366140e17a03b0de8c98
AvinashSubhash <avinashsubhash19@gmail.com>
2021-06-05 17:53:47 +0530

    added_basic_tree_search_problems

---
+++ BinaryTreeINorderTraversal.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> vec1;
+    int i=0;
+    void traverse(TreeNode* root)
+    {
+        if (root == NULL)
+            return;
+        
+        inorderTraversal(root->left);
+        vec1.insert(vec1.begin()+i,root->val);
+        i++;
+        inorderTraversal(root->right);
+        
+
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        traverse(root);
+        return vec1;
+                
+    }
+};
---
+++ BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> DataVector;
+    int i=0;
+    
+    void Traverse(TreeNode* root)
+    {
+        if (root == NULL)
+            return;
+        DataVector.insert(DataVector.begin()+i,root->val);
+        i++;
+        Traverse(root->left);
+        Traverse(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        Traverse(root);
+        return DataVector;
+    }
+};
