d0ecf47b0ccc57d10a97ab2d832f5caaa0457fd6
AvinashSubhash <avinashsubhash19@gmail.com>
2021-07-28 00:00:01 +0530

    added_prob

--- Linked-Lists/OddEvenLinkedList.cpp
+++ Linked-Lists/OddEvenLinkedList.cpp
@@ -11,41 +11,21 @@ using namespace std;
  
 class Solution {
 public:
-    ListNode* SortedList = new ListNode();
-    ListNode* temp = SortedList;
-    
-    ListNode* GroupAndAdd(ListNode* root,ListNode* temp,bool flag=true)
-    {
-        while( root != NULL)
-        {
-            if (flag==true)
-            {
-                if (root->val%2 != 0)
-                {
-                    temp->next = new ListNode(root->val);
-                    //cout<<root->val<<endl;
-                    temp = temp->next;
-                }
-            }
-            else 
-            {
-                if (root->val%2 == 0)
-                {
-                    temp->next = new ListNode(root->val);
-                    //cout<<root->val<<endl;
-                    temp = temp->next;
-                }
-            }
-            
-            root = root->next;
-        }
-        return temp;
-    }
     ListNode* oddEvenList(ListNode* head) {
-        ListNode* root = head;
-        ListNode* temp2;
-        temp2 = GroupAndAdd(root,temp);
-        temp2 = GroupAndAdd(head,temp2,false);
-        return SortedList->next;
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* t = second;
+        while(first && first->next && second && second->next){
+            first -> next = first -> next -> next;
+            if(first -> next)
+                first = first -> next;
+            second -> next = second -> next -> next;
+            second = second -> next;
+        }
+        first -> next = t; 
+        return head;
     }
 };
